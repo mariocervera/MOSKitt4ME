3515eaca875d928e675d5932b52ac8dea8bb93e9
Mario Cervera <mariocervera@gmail.com>
2016-03-22 21:14:57 +0100

    Update AssetPluginsComposite.java

--- moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/composites/AssetPluginsComposite.java
+++ moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/composites/AssetPluginsComposite.java
@@ -26,19 +26,31 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
 
+/*
+* A graphical widget that is used in the Edit Technical Fragment Dialog. It allows the user to select
+* Plug-in projects from the MOSKitt4ME workspace and include them in the technical fragment under edition.
+*
+* @author Mario Cervera
+*/
 public class AssetPluginsComposite extends Composite {
 
 	private List<IProject> plugins;
 	
 	private Group pluginsGroup;
 	
+	// Tree viewer that shows the plug-ins of the workspace
+	
 	private Label wsPluginsLabel;
 	private Tree wsPluginsTree; 
 	private TreeViewer wsPluginsTreeViewer;
 	
+	// Tree viewer that shows the plug-ins included in the fragment
+	
 	private Label assetPluginsLabel;
 	private Tree assetPluginsTree;
 	private TreeViewer assetPluginsTreeViewer;
+	
+	// Add and remove buttons
 	
 	private Button addButton;
 	private Button removeButton;
@@ -49,8 +61,10 @@ public class AssetPluginsComposite extends Composite {
 		
 		this.plugins = plugins;
 		
+		// Arrange the graphical elements of the composite (Grid layout)
+		
 		GridLayout compositeLayout = new GridLayout(1, false);
-        this.setLayout(compositeLayout);
+		this.setLayout(compositeLayout);
 		
 		pluginsGroup = new Group(this, SWT.NONE);
 		pluginsGroup.setText("Plug-ins");
@@ -74,50 +88,50 @@ public class AssetPluginsComposite extends Composite {
 		GridData gd4 = new GridData(GridData.FILL_BOTH);
 		gd4.widthHint = 200;
 		gd4.heightHint = 225;
-        wsPluginsTree.setLayoutData(gd4);
-        
-        wsPluginsTreeViewer = new TreeViewer(wsPluginsTree);
-        wsPluginsTreeViewer.setContentProvider(new WorkspacePluginsContentProvider());
-        wsPluginsTreeViewer.setLabelProvider(new WorkbenchLabelProvider());
-        wsPluginsTreeViewer.setInput(this.plugins);
-        
-        Composite c1 = new Composite(pluginsGroup, SWT.NONE);
+	        wsPluginsTree.setLayoutData(gd4);
+	        
+	        wsPluginsTreeViewer = new TreeViewer(wsPluginsTree);
+	        wsPluginsTreeViewer.setContentProvider(new WorkspacePluginsContentProvider());
+	        wsPluginsTreeViewer.setLabelProvider(new WorkbenchLabelProvider());
+	        wsPluginsTreeViewer.setInput(this.plugins);
+	        
+	        Composite c1 = new Composite(pluginsGroup, SWT.NONE);
 		GridData gd5 = new GridData(GridData.FILL_BOTH);
 		gd5.heightHint = 225;
 		c1.setLayoutData(gd5);
 		FormLayout c1Layout = new FormLayout();
 		c1.setLayout(c1Layout);
         
-        addButton = new Button(c1, SWT.PUSH);
-        addButton.setText("Add -->");
-        addButton.setFont(JFaceResources.getDialogFont());
-        FormData fd = new FormData();
-        fd.left = new FormAttachment(0, 0);
-        fd.top = new FormAttachment(38, 0);
-        fd.right = new FormAttachment(100, 0);
-        addButton.setLayoutData(fd);
+	        addButton = new Button(c1, SWT.PUSH);
+	        addButton.setText("Add -->");
+	        addButton.setFont(JFaceResources.getDialogFont());
+	        FormData fd = new FormData();
+	        fd.left = new FormAttachment(0, 0);
+	        fd.top = new FormAttachment(38, 0);
+	        fd.right = new FormAttachment(100, 0);
+	        addButton.setLayoutData(fd);
+	        
+	        removeButton = new Button(c1, SWT.PUSH);
+	        removeButton.setText("<-- Remove");
+	        removeButton.setFont(JFaceResources.getDialogFont());
+	        FormData fd2 = new FormData();
+	        fd2.left = new FormAttachment(0, 0);
+	        fd2.top = new FormAttachment(52, 0);
+	        fd2.right = new FormAttachment(100, 0);
+	        removeButton.setLayoutData(fd2);
         
-        removeButton = new Button(c1, SWT.PUSH);
-        removeButton.setText("<-- Remove");
-        removeButton.setFont(JFaceResources.getDialogFont());
-        FormData fd2 = new FormData();
-        fd2.left = new FormAttachment(0, 0);
-        fd2.top = new FormAttachment(52, 0);
-        fd2.right = new FormAttachment(100, 0);
-        removeButton.setLayoutData(fd2);
-        
-        assetPluginsTree = new Tree(pluginsGroup, SWT.BORDER | SWT.MULTI);
+        	assetPluginsTree = new Tree(pluginsGroup, SWT.BORDER | SWT.MULTI);
 		GridData gd8 = new GridData(GridData.FILL_BOTH);
 		gd8.widthHint = 200;
 		gd8.heightHint = 225;
-        assetPluginsTree.setLayoutData(gd8);
+        	assetPluginsTree.setLayoutData(gd8);
         
-        assetPluginsTreeViewer = new TreeViewer(assetPluginsTree);
-        assetPluginsTreeViewer.setContentProvider(new AssetPluginsContentProvider());
-        assetPluginsTreeViewer.setLabelProvider(new WorkbenchLabelProvider());
-        assetPluginsTreeViewer.setInput(this.plugins);
+	        assetPluginsTreeViewer = new TreeViewer(assetPluginsTree);
+	        assetPluginsTreeViewer.setContentProvider(new AssetPluginsContentProvider());
+	        assetPluginsTreeViewer.setLabelProvider(new WorkbenchLabelProvider());
+	        assetPluginsTreeViewer.setInput(this.plugins);
         
-        hookListeners();
+        	hookListeners();
 	}
 	
 	public List<IProject> getPlugins() {
