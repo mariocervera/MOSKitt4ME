c023517cf3235e126d4051daa1aaed7480023514
Mario Cervera <mariocervera@gmail.com>
2016-03-22 23:26:01 +0100

    Update InternalToolFragment.java

--- moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/util/InternalToolFragment.java
+++ moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/util/InternalToolFragment.java
@@ -2,6 +2,14 @@ package moskitt4me.repositoryClient.core.util;
 
 import java.util.List;
 
+/*
+* A class that represents a Technical Fragment of type "Internal Tool". An internal tool is a
+* tool that is already installed in MOSKitt4ME (e.g., Eclipse frameworks such as GMF or EMF).
+* This type of technical fragments do not need to encapsulate the implementation of the tools;
+* only the identifiers of the Eclipse plug-ins.
+*
+* @author Mario Cervera
+*/
 public class InternalToolFragment extends TechnicalFragment {
 
 	private String description;
