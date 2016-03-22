71560b47a670e77c00e23bc4de5eb29ea7b6653c
Mario Cervera <mariocervera@gmail.com>
2016-03-22 21:51:23 +0100

    Update DefineInternalToolDialog.java

--- moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/dialogs/DefineInternalToolDialog.java
+++ moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/dialogs/DefineInternalToolDialog.java
@@ -17,7 +17,16 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
+/*
+* A dialog that allows the user to define internal tools. An internal tool is a tool that is already
+* installed in MOSKitt4ME (e.g., Eclipse frameworks such as GMF or EMF). This type of technical fragments
+* do not need to encapsulate the implementation of the tools; only the identifiers of the Eclipse plug-ins.
+*
+* @author Mario Cervera
+*/
 public class DefineInternalToolDialog extends Dialog {
+
+	//Fragment properties (name, origin, objective, etc.)
 
 	private String name;
 	private Text nameText;
@@ -39,8 +48,12 @@ public class DefineInternalToolDialog extends Dialog {
 	
 	private InternalToolPluginsComposite pluginsComposite;
 	
+	//Ok and Cancel buttons
+	
 	protected Button okButton;
 	protected Button cancelButton;
+	
+	//The fragment to be stored in the repository
 	
 	private InternalToolFragment result;
 	
@@ -93,7 +106,8 @@ public class DefineInternalToolDialog extends Dialog {
 		super.configureShell(shell);
         
         shell.setText("Define Internal Tool");
-    }
+		
+	}
 	
 	protected void createButtonsForButtonBar(Composite parent) {
         
@@ -104,78 +118,79 @@ public class DefineInternalToolDialog extends Dialog {
     	
     	cancelButton = createButton(parent, IDialogConstants.CANCEL_ID,
                 IDialogConstants.CANCEL_LABEL, false);
-    }
+		
+	}
 	
 	protected Control createDialogArea(Composite parent) {
 
 		Composite composite = (Composite) super.createDialogArea(parent);
-        GridLayout compositeLayout = new GridLayout(2, false);
-        composite.setLayout(compositeLayout);
+	        GridLayout compositeLayout = new GridLayout(2, false);
+	        composite.setLayout(compositeLayout);
+	        
+	        Label nameLabel = new Label(composite, SWT.NONE);
+	        nameLabel.setText("Name:");
+	        GridData gd = new GridData();
+	        nameLabel.setLayoutData(gd);
+	        
+	        nameText = new Text(composite, SWT.BORDER);
+	        GridData gd2 = new GridData(GridData.FILL_HORIZONTAL);
+	        gd2.widthHint = 350;
+	        nameText.setLayoutData(gd2);
         
-        Label nameLabel = new Label(composite, SWT.NONE);
-        nameLabel.setText("Name:");
-        GridData gd = new GridData();
-        nameLabel.setLayoutData(gd);
+	        Label originLabel = new Label(composite, SWT.NONE);
+	        originLabel.setText("Origin:");
+	        GridData gd3 = new GridData();
+	        nameLabel.setLayoutData(gd3);
+	        
+	        originText = new Text(composite, SWT.BORDER);
+	        GridData gd4 = new GridData(GridData.FILL_HORIZONTAL);
+	        originText.setLayoutData(gd4);
+	        
+	        Label objectiveLabel = new Label(composite, SWT.NONE);
+	        objectiveLabel.setText("Objective:");
+	        GridData gd5 = new GridData();
+	        objectiveLabel.setLayoutData(gd5);
+	        
+	        objectiveText = new Text(composite, SWT.BORDER);
+	        GridData gd6 = new GridData(GridData.FILL_HORIZONTAL);
+	        objectiveText.setLayoutData(gd6);
+	        
+	        Label inputLabel = new Label(composite, SWT.NONE);
+	        inputLabel.setText("Input:");
+	        GridData gd7 = new GridData();
+	        inputLabel.setLayoutData(gd7);
         
-        nameText = new Text(composite, SWT.BORDER);
-        GridData gd2 = new GridData(GridData.FILL_HORIZONTAL);
-        gd2.widthHint = 350;
-        nameText.setLayoutData(gd2);
+	        inputText = new Text(composite, SWT.BORDER);
+	        GridData gd8 = new GridData(GridData.FILL_HORIZONTAL);
+	        inputText.setLayoutData(gd8);
+	        
+	        Label outputLabel = new Label(composite, SWT.NONE);
+	        outputLabel.setText("Output:");
+	        GridData gd9 = new GridData();
+	        outputLabel.setLayoutData(gd9);
+	        
+	        outputText = new Text(composite, SWT.BORDER);
+	        GridData gd10 = new GridData(GridData.FILL_HORIZONTAL);
+	        outputText.setLayoutData(gd10);
+	        
+	        Label descriptionLabel = new Label(composite, SWT.NONE);
+	        descriptionLabel.setText("Description:");
+	        GridData gd11 = new GridData();
+	        descriptionLabel.setLayoutData(gd11);
         
-        Label originLabel = new Label(composite, SWT.NONE);
-        originLabel.setText("Origin:");
-        GridData gd3 = new GridData();
-        nameLabel.setLayoutData(gd3);
-        
-        originText = new Text(composite, SWT.BORDER);
-        GridData gd4 = new GridData(GridData.FILL_HORIZONTAL);
-        originText.setLayoutData(gd4);
-        
-        Label objectiveLabel = new Label(composite, SWT.NONE);
-        objectiveLabel.setText("Objective:");
-        GridData gd5 = new GridData();
-        objectiveLabel.setLayoutData(gd5);
-        
-        objectiveText = new Text(composite, SWT.BORDER);
-        GridData gd6 = new GridData(GridData.FILL_HORIZONTAL);
-        objectiveText.setLayoutData(gd6);
-        
-        Label inputLabel = new Label(composite, SWT.NONE);
-        inputLabel.setText("Input:");
-        GridData gd7 = new GridData();
-        inputLabel.setLayoutData(gd7);
-        
-        inputText = new Text(composite, SWT.BORDER);
-        GridData gd8 = new GridData(GridData.FILL_HORIZONTAL);
-        inputText.setLayoutData(gd8);
-        
-        Label outputLabel = new Label(composite, SWT.NONE);
-        outputLabel.setText("Output:");
-        GridData gd9 = new GridData();
-        outputLabel.setLayoutData(gd9);
-        
-        outputText = new Text(composite, SWT.BORDER);
-        GridData gd10 = new GridData(GridData.FILL_HORIZONTAL);
-        outputText.setLayoutData(gd10);
-        
-        Label descriptionLabel = new Label(composite, SWT.NONE);
-        descriptionLabel.setText("Description:");
-        GridData gd11 = new GridData();
-        descriptionLabel.setLayoutData(gd11);
-        
-        descriptionText = new Text(composite, SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
-        GridData gd12 = new GridData(GridData.FILL_BOTH);
-        gd12.heightHint = 100;
-        descriptionText.setLayoutData(gd12);
-        
-        pluginsComposite = new InternalToolPluginsComposite(composite, SWT.NONE, this);
-        GridData gd13 = new GridData(GridData.FILL_BOTH);
-        gd13.horizontalSpan = 2;
-        pluginsComposite.setLayoutData(gd13);
-        
-        hookListeners();
-        
-        return composite;
+	        descriptionText = new Text(composite, SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
+	        GridData gd12 = new GridData(GridData.FILL_BOTH);
+	        gd12.heightHint = 100;
+	        descriptionText.setLayoutData(gd12);
+	        
+	        pluginsComposite = new InternalToolPluginsComposite(composite, SWT.NONE, this);
+	        GridData gd13 = new GridData(GridData.FILL_BOTH);
+	        gd13.horizontalSpan = 2;
+	        pluginsComposite.setLayoutData(gd13);
+	        
+	        hookListeners();
+	        
+	        return composite;
 	}
 	
 	protected void hookListeners() {
