e014c74ad8ab67b970ecffe77d65a4ac0b34c842
Mario Cervera <mariocervera@gmail.com>
2016-03-22 01:16:25 +0100

    Update DefineInternalToolAction.java

--- moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/actions/DefineInternalToolAction.java
+++ moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/actions/DefineInternalToolAction.java
@@ -21,6 +21,13 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.ui.PlatformUI;
 
+/*
+* In a similar way to the class "CreateTechnicalFragmentAction", this class implements an action that
+* enables the creation of technical fragments. The difference is that this class enables the creation of
+* technical fragments of type "Internal Tool". An internal tool is a tool that is already installed in
+* MOSKitt4ME (e.g., Eclipse frameworks such as GMF or EMF). This type of technical fragments do not need
+* to encapsulate the implementation of the tools; only the identifiers of the Eclipse plug-ins.
+*/ 
 public class DefineInternalToolAction extends Action {
 
 	@Override
