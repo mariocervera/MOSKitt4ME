5daf1d16b2663e6f5eb2efa5833ee221caa6112c
Mario Cervera <mariocervera@gmail.com>
2016-03-23 23:23:08 +0100

    Update ProductExplorerViewActionGroup.java

--- moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/actions/ProductExplorerViewActionGroup.java
+++ moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/actions/ProductExplorerViewActionGroup.java
@@ -3,8 +3,10 @@ package moskitt4me.projectmanager.core.actions;
 import org.eclipse.jface.viewers.TreeViewer;
 
 /*
- * This class defines the actions that appear in the action bars of the Product Explorer
- * view and are not common to all the other views in the project manager
+ * This class defines the actions that appear in the Action Bar of the Product Explorer
+ * view and are not common to all the other views in the Project Manager Component.
+ *
+ * @author Mario Cervera
  */
 public class ProductExplorerViewActionGroup extends ProjectManagerActionGroup {
 	
