ce2e9cee8cfa3819e2ebc269e1c137d33c1e8f22
Mario Cervera <mariocervera@gmail.com>
2016-03-23 23:11:06 +0100

    Update GuidesViewActionGroup.java

--- moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/actions/GuidesViewActionGroup.java
+++ moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/actions/GuidesViewActionGroup.java
@@ -3,8 +3,10 @@ package moskitt4me.projectmanager.core.actions;
 import org.eclipse.jface.viewers.TreeViewer;
 
 /*
- * This class defines the actions that appear in the action bars of the Guides view
+ * This class defines the actions that appear in the action bar of the Guides view
  * and are not common to all the other views in the project manager
+ *
+ * @author Mario Cervera
  */
 public class GuidesViewActionGroup extends ProjectManagerActionGroup {
 
