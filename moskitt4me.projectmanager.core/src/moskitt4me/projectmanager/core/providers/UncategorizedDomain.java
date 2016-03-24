1bfb4624c396f78de4d4322fc99c7ec0610fad09
Mario Cervera <mariocervera@gmail.com>
2016-03-24 03:24:11 +0100

    Update UncategorizedDomain.java

--- moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/providers/UncategorizedDomain.java
+++ moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/providers/UncategorizedDomain.java
@@ -2,6 +2,9 @@ package moskitt4me.projectmanager.core.providers;
 
 import org.eclipse.epf.uma.impl.DomainImpl;
 
+/*
+* @author Mario Cervera
+*/
 public class UncategorizedDomain extends DomainImpl {
 
 	public UncategorizedDomain() {
