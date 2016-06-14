75059a29339eb1fc1fcb5389ff8995c67552984d
Mario Cervera <mariocervera@gmail.com>
2016-06-14 19:48:13 +0200

    Update InternalToolFragment.java

--- moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/util/InternalToolFragment.java
+++ moskitt4me.repositoryClient.core/src/moskitt4me/repositoryClient/core/util/InternalToolFragment.java
@@ -2,19 +2,22 @@ package moskitt4me.repositoryClient.core.util;
 
 import java.util.List;
 
-/*
-* A class that represents a Technical Fragment of type "Internal Tool". An internal tool is a
-* tool that is already installed in MOSKitt4ME (e.g., Eclipse frameworks such as GMF or EMF).
-* This type of technical fragments do not need to encapsulate the implementation of the tools;
-* only the identifiers of the Eclipse plug-ins.
-*
-* @author Mario Cervera
-*/
+/**
+ * A class that represents a Technical Fragment of type "Internal Tool". An internal tool is a
+ * tool that is already installed in MOSKitt4ME (e.g., Eclipse frameworks such as GMF or EMF).
+ * This type of technical fragments do not need to encapsulate the implementation of the tools;
+ * only the identifiers of the Eclipse plug-ins.
+ *
+ * @author Mario Cervera
+ */
 public class InternalToolFragment extends TechnicalFragment {
 
-	private String description;
-	private List<String> pluginIds;
+	private String description; // A description of the tool
+	private List<String> pluginIds; // References to the plug-ins that implement the internal tool
 	
+	/*
+	 * Constructor
+	 */
 	public InternalToolFragment(String name, String origin, String objective,
 			String input, String output, String description, List<String> pluginIds) {
 		
@@ -24,6 +27,8 @@ public class InternalToolFragment extends TechnicalFragment {
 		this.pluginIds = pluginIds;
 		
 	}
+	
+	// Getters and setters
 	
 	public String getDescription() {
 		return description;
