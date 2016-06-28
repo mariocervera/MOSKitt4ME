6844e43905a158bf67a9568cbf312713d8625941
Mario Cervera <mariocervera@gmail.com>
2016-06-28 23:56:07 +0200

    Update HelpAction.java

--- moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/actions/HelpAction.java
+++ moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/actions/HelpAction.java
@@ -5,19 +5,25 @@ import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.PlatformUI;
 
-/*
-* An action that is available in the Action Bar of the Process view and provides
-* textual assistance for the user.
-*
-* @author Mario Cervera
-*/
+/**
+ * An action that is available in the Action Bar of the Process view and provides
+ * textual assistance for the user.
+ *
+ * @author Mario Cervera
+ */
 public class HelpAction extends Action implements IAction {
 
+	/*
+	 * Constructor
+	 */
 	public HelpAction() {
 		
 		super("Help");
 	}
 
+	/*
+	 * The action just displays an informative message dialog
+	 */
 	public void run() {
 		
 		String message = "Colours indicate states of tasks and activities:\n\n" +
