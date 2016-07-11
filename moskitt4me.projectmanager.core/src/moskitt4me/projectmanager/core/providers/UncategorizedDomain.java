840dee72c18d14dcc54ab58d3cead03dbc5a1695
Mario Cervera <mariocervera@gmail.com>
2016-07-11 18:12:13 +0200

    Update UncategorizedDomain.java

--- moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/providers/UncategorizedDomain.java
+++ moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/providers/UncategorizedDomain.java
@@ -2,9 +2,12 @@ package moskitt4me.projectmanager.core.providers;
 
 import org.eclipse.epf.uma.impl.DomainImpl;
 
-/*
-* @author Mario Cervera
-*/
+/**
+ * The "Uncategorized" domain of the Product Explorer view. It contains all the products that are not
+ * contained in any domain (that is, the products that are uncategorized)
+ * 
+ * @author Mario Cervera
+ */
 public class UncategorizedDomain extends DomainImpl {
 
 	public UncategorizedDomain() {
