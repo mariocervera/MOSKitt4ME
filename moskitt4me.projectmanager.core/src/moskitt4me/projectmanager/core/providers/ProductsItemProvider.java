152cc173d2a659b3927db71480036eb67a359ea4
Mario Cervera <mariocervera@gmail.com>
2016-07-11 17:59:46 +0200

    Update ProductsItemProvider.java

--- moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/providers/ProductsItemProvider.java
+++ moskitt4me.projectmanager.core/src/moskitt4me/projectmanager/core/providers/ProductsItemProvider.java
@@ -3,9 +3,11 @@ package moskitt4me.projectmanager.core.providers;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 
-/*
-* @author Mario Cervera
-*/
+/**
+ * The root element of the Product Explorer view
+ * 
+ * @author Mario Cervera
+ */
 public class ProductsItemProvider extends ItemProviderAdapter  {
 
 	public ProductsItemProvider(AdapterFactory adapterFactory) {
